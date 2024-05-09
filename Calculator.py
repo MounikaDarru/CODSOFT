7fd9a16169330ae8602f7460dfa20634c9b51d1f
MounikaDarru <darru.mounika.123@gmail.com>
2024-05-09 16:56:20 +0530

    Modified Code

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,5 @@
 import os, sys
-print("--Calculator--")
+print("----Calculator----")
 result = int(input("Enter a Numbers: "))
 while True:
     os.system("clear")
@@ -8,7 +8,7 @@ while True:
     choice = input("Addition\nSubtraction\nMultiplication\nDivision\nExit\n\nEnter Your Choice : ")
     if choice in ["Exit",'EXIT','EXIT','exit','Exit','e','E']:
       os.system("clear")
-      sys.exit(f"--END--\n\nRESULT : {result}")
+      sys.exit(f"----END----\n\nFINAL RESULT : {result}")
     else:
       y = int(input("Enter Number : "))
       if choice in ["Addition",'ADDITION','ADD','add','Add','+','addition']:
