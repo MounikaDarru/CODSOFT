38439739595020fe1aefe0016f2f50aafb111714
MounikaDarru <darru.mounika.123@gmail.com>
2024-05-09 17:34:46 +0530

    Create Calculator

--- Calculator.py
+++ Calculator.py
@@ -2,25 +2,26 @@ import os, sys
 print("----Calculator----")
 result = int(input("Enter a Numbers: "))
 while True:
-    os.system("clear")
+    os.system('cls' if os.name == 'nt' else 'clear')
     print("----Calculator----")
     print("\nResult : ",result,"\n")
     choice = input("Addition\nSubtraction\nMultiplication\nDivision\nExit\n\nEnter Your Choice : ")
-    if choice in ["Exit",'EXIT','EXIT','exit','Exit','e','E']:
-      os.system("clear")
+    choice.lower()
+    if choice in ['exit','e']:
+      os.system('cls' if os.name == 'nt' else 'clear')
       sys.exit(f"----END----\n\nFINAL RESULT : {result}")
     else:
       y = int(input("Enter Number : "))
-      if choice in ["Addition",'ADDITION','ADD','add','Add','+','addition']:
+      if choice in ['add','+','addition']:
         result += y
         print("\nResult : ",result)
-      elif choice in ["Subtraction",'SUBTRACTION','SUB','sub','Sub','-','subtraction']:
+      elif choice in ['sub','-','subtraction']:
         result -= y
         print("\nResult : ",result)
-      elif choice in ["Multiplication",'MULTIPLICATION','MUL','mul','Mul','*','multiplication']:
+      elif choice in ['mul','*','multiplication']:
         result *= y
         print("\nResult : ",result)
-      elif choice in ["Division",'DIVISION','DIV','div','Div','/','division']:
+      elif choice in ['div','/','division']:
         result /= y
         print("\nResult : ",result)
   
